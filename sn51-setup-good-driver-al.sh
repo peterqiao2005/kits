1a083ab389f614716a72081da0f2ef4c01f21376
peter qiao <peterqiao2005@gmail.com>
2025-01-29 06:39:24 +0800

    Update sn51-setup-good-driver-al.sh

--- sn51-setup-good-driver-al.sh
+++ sn51-setup-good-driver-al.sh
@@ -42,8 +42,8 @@ git clone https://github.com/Datura-ai/compute-subnet.git
 cd compute-subnet && chmod +x scripts/install_executor_on_ubuntu.sh && scripts/install_executor_on_ubuntu.sh
 
 cd ~/workspace/sn51/compute-subnet/neurons/executor
-curl -fsSL https://raw.githubusercontent.com/peterqiao2005/kits/main/executor-al/docker-compose.app.yml -o docker-compose.app.yml
-curl -fsSL https://raw.githubusercontent.com/peterqiao2005/kits/main/executor-al/.env -o .env
+curl -fsSL https://raw.githubusercontent.com/peterqiao2005/kits/main/executor_al/docker-compose.app.yml -o docker-compose.app.yml
+curl -fsSL https://raw.githubusercontent.com/peterqiao2005/kits/main/executor_al/.env -o .env
 
 docker compose -f docker-compose.app.yml up -d
 
