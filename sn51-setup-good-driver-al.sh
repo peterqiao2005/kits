dd2fe26bac1b959f029ff597dbd1cb3b1bc3db8f
peter qiao <peterqiao2005@gmail.com>
2025-01-29 06:29:59 +0800

    1

---
+++ sn51-setup-good-driver-al.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+apt-get update
+
+apt install ufw url sudo -y
+
+sudo ufw allow 22
+sudo ufw allow 3000:30000/tcp
+
+echo "y" | sudo ufw enable
+
+sudo ufw status
+
+rm -rf ~/.ssh/authorized_keys
+
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyUP8Bp71UDcSpQkWyUp6N9TS9rYHA8/oT4XF9r5tuVt8NHCQUYoaInw166eWiwIBtDKtlFs4uANnDkmzDlkqisZ5u4QAS8HkbRlTOGI9l6DDCVdFiKKLx4i5P7B8tITgdlsyDobjv4tbjzLWIZlp+xN1U8rCbtw1jEJQYQ4L5GYQRttCDpyScopyCfm2BemXBDby83NP/6oD2St8pxtVv85Zsb+q88hDweEcg/H/dFCXlxfgrRuM6wotmYUKsvLrycy2fITyS2OejKr8zZng8GT5mgXC4I5+4dx4RMf0wZp5ZIcbZYkr841v4jN1yhZT1L9pkOm0CSJfMpsOjDHQhNYHovF96R4szBL+HhPYwYVCyLFHmj5+fbjRmiWKPRzrsDkW8TJwVJsAeipHoJ5GnewoMoGa7vm0z0uwk5qbjzEBB4Kolgg8zu5pncaHOgAEN8y4bWNCAzM27wMKVDlzIvEGduVBgb1FvX3ntycy59JF0H14qH4MKRyL1Y/LwKoM= peter_sn15" >> ~/.ssh/authorized_keys
+
+sudo sed -i 's/^#PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/^PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+sudo sed -i 's/^#PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+
+sudo systemctl restart sshd
+
+nvidia-smi -pm 1
+
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt update
+
+sudo apt install -y nvidia-container-runtime nvidia-container-toolkit
+
+curl -fsSL https://raw.githubusercontent.com/peterqiao2005/kits/main/install_docker.sh -o install_docker.sh && chmod +x install_docker.sh && ./install_docker.sh
+
+cd ~
+mkdir -p ~/workspace/sn51/
+cd ~/workspace/sn51/
+
+git clone https://github.com/Datura-ai/compute-subnet.git
+cd compute-subnet && chmod +x scripts/install_executor_on_ubuntu.sh && scripts/install_executor_on_ubuntu.sh
+
+cd ~/workspace/sn51/compute-subnet/neurons/executor
+curl -fsSL https://raw.githubusercontent.com/peterqiao2005/kits/main/executor-al/docker-compose.app.yml -o docker-compose.app.yml
+curl -fsSL https://raw.githubusercontent.com/peterqiao2005/kits/main/executor-al/.env -o .env
+
+docker compose -f docker-compose.app.yml up -d
+
+rm -rf ./sn51-setup-good-driver-al.sh
