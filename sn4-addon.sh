1f5bb3e576d66f5568186bc083a148ebab295c23
peter qiao <peterqiao2005@gmail.com>
2025-03-03 14:14:58 +0800

    Update sn4-addon.sh

--- sn4-addon.sh
+++ sn4-addon.sh
@@ -9,7 +9,10 @@ sudo apt install -y nvidia-docker2
 	
 systemctl restart docker
 
-docker run --restart always --runtime nvidia --gpus all -d -p 8844:8000 manifoldlabs/targon-goggles:v1
+docker ps -aq --filter ancestor=manifoldlabs/targon-goggles:v1 | xargs -r docker stop && docker ps -aq --filter ancestor=manifoldlabs/targon-goggles:v1 | xargs -r docker rm
+
+docker run --name targon-goggles --restart always --runtime nvidia --gpus all -d -p 8844:8000 manifoldlabs/targon-goggles:v1
+( crontab -l ; echo "*/10 * * * * /usr/bin/docker restart targon-goggles" ) | crontab -
 
 # 自我删除
 rm -- "$SCRIPT_PATH"
