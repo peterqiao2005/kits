c9c5c188e154975bc21e6f7157bbabb61b5b20a7
peter qiao <peterqiao2005@gmail.com>
2025-03-08 01:29:18 +0800

    Update sn4-addon.sh

--- sn4-addon.sh
+++ sn4-addon.sh
@@ -12,7 +12,26 @@ SCRIPT_PATH=$(readlink -f "$0")
 docker ps -aq --filter ancestor=manifoldlabs/targon-goggles:v1 | xargs -r docker stop && docker ps -aq --filter ancestor=manifoldlabs/targon-goggles:v1 | xargs -r docker rm
 
 docker run --name targon-goggles --restart always --runtime nvidia --gpus all -d -p 8844:8000 manifoldlabs/targon-goggles:v1
-( crontab -l ; echo "*/10 * * * * /usr/bin/docker restart targon-goggles" ) | crontab -
+
+# 1. 确保目录存在
+mkdir -p /root/workspace/sn4
+
+# 2. 将脚本内容写入 /root/workspace/sn4/check_goggles.sh
+cat << 'EOF' > /root/workspace/sn4/check_goggles.sh
+#!/bin/bash
+RESPONSE=$(curl -s localhost:8844 --data '{"nonce": ""}')
+if ! echo "$RESPONSE" | grep -q '"msg"' || ! echo "$RESPONSE" | grep -q '"no_of_gpus":8'; then
+  docker kill targon-goggles
+  sleep 5
+  docker run --runtime nvidia --gpus all -d --name targon-goggles -p 8844:8000 manifoldlabs/targon-goggles:v1
+fi
+EOF
+
+# 3. 赋予脚本可执行权限
+chmod +x /root/workspace/sn4/check_goggles.sh
+
+( crontab -l | grep -v "docker restart targon-goggles" ; echo "* * * * * /root/workspace/sn4/check_goggles.sh" ) | crontab -
+# ( crontab -l ; echo "* * * * * /root/workspace/sn4/check_goggles.sh" ) | crontab -
 
 # 自我删除
 rm -- "$SCRIPT_PATH"
