42d15e3296cb8ac5320650cc38826a2106e75a68
peterqiao2005 <peterqiao2005@gmail.com>
2025-08-21 19:28:48 +0800

    add function to save record to db

---
+++ serverroomPrice/pm2.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: "serverroomPrice",
+      script: "dicountMonitoringNew.py",
+      interpreter: "python3",
+      cron_restart: "0 */4 * * *",   // 每 4 小时执行一次
+      autorestart: false,            // 执行完就退出，不常驻
+    },
+  ]
+};
