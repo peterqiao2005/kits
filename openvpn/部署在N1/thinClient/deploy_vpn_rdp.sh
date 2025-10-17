cb21b43c56a1f96939840367e80ba153a78dc76c
peter qiao <peterqiao2005@gmail.com>
2025-10-17 09:04:58 +0800

    1

--- openvpn/部署在N1/thinClient/deploy_vpn_rdp.sh
+++
@@ -1,8 +0,0 @@
-sudo RDP_USER='ai' \
-     RDP_PASS='32167' \
-     RDP_HOST='192.168.100.105' \
-     OVPN_FILE='/root/client.ovpn' \
-     OVPN_NAME='vpn-jb' \
-     LOCAL_USER='root' \
-     RDP_FULL=1 \
-     bash ./setup_armbian_vpn_rdp.sh
